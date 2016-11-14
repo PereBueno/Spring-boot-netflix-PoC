a6b9b83bbb6f31fd099ab61d548ca546b2f524b4
PereBueno <pere.bueno@gmail.com>
2016-11-14 20:00:40 +0100

    Reduced base class to just starting the app

--- src/main/java/com/pbueno/tutos/spring/cloud/netflix/App.java
+++ src/main/java/com/pbueno/tutos/spring/cloud/netflix/App.java
@@ -2,20 +2,16 @@ package com.pbueno.tutos.spring.cloud.netflix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by Pedro on 14/11/2016.
  */
+@Configuration
 @RestController
 @EnableAutoConfiguration
 public class App {
-
-    @RequestMapping("/")
-    String home() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(App.class, args);
